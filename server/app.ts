a9d8f9159bab09588a582b276059bda9480818d2
tanjim00 <tanjim.ac00@gmail.com>
2024-02-26 12:28:33 +0600

    node modules bug fix

--- server/app.ts
+++ server/app.ts
@@ -1,13 +1,33 @@
-import express from "express";
+require('dotenv').config();
+import express,{Request, Response, NextFunction} from "express";
 export const app = express();
 import cors from "cors";
-import cookieparser from "cookie-parser";
+import cookieParser from "cookie-parser";
+
 
 //body parser
 app.use(express.json({limit: "50mb"}));
 
 //cookie parser
-app.use(cookieparser());
+app.use(cookieParser());
 
 //cors
-app.use(cors());
+app.use(cors({
+    origin: process.env.ORIGIN
+}));
+
+//testing api
+app.get("/test", (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).json({
+        success: true,
+        message: "API is working",
+
+    });
+});
+
+//unknown route
+app.all("*",(req: Request, res: Response, next:NextFunction) => {
+    const err = new Error(`Route ${req.originalUrl} not found`) as any;
+    err.statusCode = 404;
+    next(err);
+});
